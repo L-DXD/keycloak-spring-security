d918483738350a80fbb0f098929d9e47c991c0f0
BongSeungLee <yui5227@idstrust.com>
2026-01-05 09:58:40 +0900

    feat(*) : 05.서블릿 oidc 로그인 흐름 구현

--- integration-tests/servlet-app/src/main/java/com/ids/keycloak/security/test/servlet/TestController.java
+++ integration-tests/servlet-app/src/main/java/com/ids/keycloak/security/test/servlet/TestController.java
@@ -1,13 +1,19 @@
 package com.ids.keycloak.security.test.servlet;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "OK";
+    @GetMapping("/api/me")
+    public ResponseEntity<String> getMyInfo(@AuthenticationPrincipal OAuth2User principal) {
+        if (principal != null) {
+            return ResponseEntity.ok("Hello, " + principal.getName());
+        }
+        return ResponseEntity.status(401).body("Unauthorized");
     }
 }
