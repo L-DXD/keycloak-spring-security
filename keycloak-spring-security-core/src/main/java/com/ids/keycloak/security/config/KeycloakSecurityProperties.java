8d4c207e150f5eb06c5c283408eaff54cb361b1e
BongSeungLee <yui5227@idstrust.com>
2026-01-15 15:17:21 +0900

    refactor(*) : 환경 변수 수정

--- keycloak-spring-security-core/src/main/java/com/ids/keycloak/security/config/KeycloakSecurityProperties.java
+++ keycloak-spring-security-core/src/main/java/com/ids/keycloak/security/config/KeycloakSecurityProperties.java
@@ -63,4 +63,10 @@ public class KeycloakSecurityProperties {
      */
     @NestedConfigurationProperty
     private KeycloakLoggingProperties logging = new KeycloakLoggingProperties();
+
+    /**
+     * Session 관련 설정
+     */
+    @NestedConfigurationProperty
+    private KeycloakSessionProperties session = new KeycloakSessionProperties();
 }
