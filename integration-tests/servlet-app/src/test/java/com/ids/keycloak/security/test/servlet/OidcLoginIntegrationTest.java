ab6da173a9e8fb017dae06f2b5a893e05c176fe4
BongSeungLee <yui5227@idstrust.com>
2026-01-15 10:51:57 +0900

    ci/cd(*) : maven repo 배포 설정

--- integration-tests/servlet-app/src/test/java/com/ids/keycloak/security/test/servlet/OidcLoginIntegrationTest.java
+++ integration-tests/servlet-app/src/test/java/com/ids/keycloak/security/test/servlet/OidcLoginIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.ids.keycloak.security.test.servlet.support.WithMockOidcLogin;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
